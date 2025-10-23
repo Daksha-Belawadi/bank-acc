0dd564679c81d000c77fb64c5dd601eb3683cf25
Daksha-Belawadi <belawadidaksha@gmailcom>
2025-10-23 15:53:49 +0530

    save to branch1

--- bank.py
+++ bank.py
@@ -2,3 +2,9 @@ b=int(input("Balance: "))
 d=int(input("Deposit: "))
 b+=d
 print("New Balance:",b)
+b=int(input("Balance: "))
+d=int(input("Deposit: "))
+b+=d
+w=int(input("Withdraw: "))
+b-=w
+print("Final Balance:",b)
